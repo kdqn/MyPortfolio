a55a55d363446010474762dd6643f93590a9e845
Cayden <cayden.odegard@gmail.com>
2024-12-18 23:03:40 -0600

    sql testing

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "<h1> hello sunshine!</h1>";
 echo "<p>this is my portfolio page (not yet but it works for now)</p>";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+print($conn);
 // Check connection
 if ($conn->connect_error) {
     echo"Connection failed: " . $conn->connect_error;
