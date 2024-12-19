69c31fe26be1e88ac6f2cd435beeadbc2eb35766
Cayden <cayden.odegard@gmail.com>
2024-12-18 23:00:56 -0600

    sql testing

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<h1>hello sunshine!</h1>
-<p>this is my portfolio page (not yet but yk what I mean)</p>
-  
-
 <?php
 $servername = "162.0.209.84";
 $username = "branqspi_kdqn";
 $password = "CMO4mysql!";
 $dbname = "branqspi_portfolio";
-
+echo "<h1> hello sunshine!</h1>";
+echo "<p>this is my portfolio page (not yet but yk what I mean)</p>";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
