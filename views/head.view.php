15a087e268cccbb78f72195af347add58fb977c5
Cayden <cayden.odegard@gmail.com>
2025-01-01 13:48:26 -0600

    added favicon

--- views/head.view.php
+++ views/head.view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="<?php echo URL_ROOT . '/style.css' ?>">
-<link rel="icon" type="image/x-icon" href="<?php echo URL_ROOT . '/media/favicon.io' ?>">
+<link rel="icon" type="image/x-icon" href="<?php echo URL_ROOT . '/media/favicons-basiclogo/favicon.io' ?>">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
