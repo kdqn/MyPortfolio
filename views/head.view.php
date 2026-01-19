ee963580a7b57e4dca5103deeae7056bc393e682
Cayden <cayden.odegard@gmail.com>
2026-01-18 19:44:59 -0600

    app root changes

--- views/head.view.php
+++ views/head.view.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="<?php echo APP_ROOT . '/style.css' ?>">
+<link rel="stylesheet" href="<?php echo URL_ROOT . '/style.css' ?>">
 <link rel="icon" type="image/x-icon" href="<?php echo URL_ROOT . '/media/favicons-basiclogo/favicon.ico' ?>">
 <head>
 	<meta charset="UTF-8">
@@ -8,4 +8,4 @@
 	<title><?php echo $data[0]['page'] ?></title>
 </head>
 <body>
-<?php include_once APP_ROOT . '/views/header.view.php' ?>
+<?php include_once('header.view.php')?>
